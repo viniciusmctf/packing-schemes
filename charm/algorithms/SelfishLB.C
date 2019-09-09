1d0d9302adf696afc67bafed9c4dc1ca21dad614
viniciusmctf <vinimmbb@gmail.com>
2019-09-09 14:40:51 +0200

    First draft of Selfish algorithm

--- charm/algorithms/SelfishLB.C
+++ charm/algorithms/SelfishLB.C
@@ -1,6 +1,12 @@
 #include "SelfishLB.h"
 #include <random>
 
+void __control_print(const char* string) {
+  if (CkMyPe() == 0 && _lb_args.debug() > 1) {
+    CkPrintf("%s\n", string);
+  }
+}
+
 CreateLBFunc_Def(SelfishLB, "Greedy Selfish Task Reallocation");
 
 SelfishLB::SelfishLB(CkMigrateMessage *m) : CBase_SelfishLB(m) {
@@ -23,8 +29,10 @@ void SelfishLB::Strategy(const DistBaseLB::LDStats* const stats) {
   }
 
   gen(rd()); // Set random device
-  kIterations = 10;
-  waiting_messages = 0;
+  kIterations = 10; cur_iteration = 1;
+  mig_counts.clear();
+  mig_counts = std::vector<int> (CkNumPes(), 0); // Fill mig_counts with 0s
+  waiting_messages = 0; migrates_expected = 0;
   my_load = 0.0;
   all_tasks.clear(); migrateInfo.clear();
   my_stats = stats;
@@ -36,7 +44,7 @@ void SelfishLB::Strategy(const DistBaseLB::LDStats* const stats) {
 
   for (int i = 0; i < nobjs; ++i) {
     if (my_stats->objs[i].wallTime > 0.0001 && my_stats->objs[i].migratable) {
-      task_to_destination_map.emplace({my_id, my_id, i}); // Final destination map
+      // task_to_destination_map.emplace({my_id, my_id, i}); // Final destination map
       all_tasks.emplace({my_id, my_id, i, my_stats->objs[i].wallTime}); // Vector of temporary migration tokens
     }
   }
@@ -60,12 +68,10 @@ void SelfishLB::CalculateMigrations() {
 
 void SelfishLB::ReceiveTask(int host_id, double host_load, int task_id, double task_load,
     int arr_position) {
-  my_load += task_load;
-  bool receive = DetermineMigrationSuccess(host_load);
+  bool receive = DetermineMigrationSuccess(host_load, task_load);
   if (receive) {
+    my_load += task_load;
     all_tasks.emplace({host_id, CkMyPe(), task_id, task_load});
-  } else {
-    my_load -= task_load;
   }
   thisProxy[host_id].ConfirmRecv(receive, CkMyPe(), arr_position);
 }
@@ -79,6 +85,105 @@ void SelfishLB::ConfirmRecv(bool recv, int who, int arr_position) {
     my_load -= task_load;
   }
   if (!waiting_messages) {
-    // Contribute for Iteration end
+    cur_iteration++;
+    contribute(CkCallback(CkReductionTarget(SelfishLB, NextIteration), thisProxy));
   }
+}
+
+void SelfishLB::NextIteration() {
+  __control_print(".");
+  if (cur_iteration >= kIterations) {
+    // Prepare to inform moves of tasks in remote peers
+    std::map< int, std::map<int, int> > remote_task_map; // PE_ID -> Task -> Target
+    for (auto task : all_tasks) {
+      if (std::get<0>(task) != CkMyPe()) {
+        remote_task_map[std::get<0>(task)][std::get<2>(task)] = std::get<1>(task);
+      }
+    }
+    for (auto entry : remote_task_map) {
+      int remote_host = entry.first;
+      int msg_size = entry.second.size();
+      int* tids = new int[msg_size]; int* targets = new int[msg_size];
+      // This could be optimized to one contiguous array, accessing 2 by 2, but the code would be less readable
+      auto it = entry.second.begin();
+      for (int i = 0; i < msg_size; i++, it++) {
+        task_target = (std::pair<int,int>)(*it);
+        tids[i] = task_target.first; targets[i] = task_target.second;
+      }
+      thisProxy[remote_host].MoveTasks(CkMyPe(), msg_size, tids, targets);
+      waiting_messages++;
+    }
+    if (!waiting_messages){
+      contribute(CkCallback(CkReductionTarget(SelfishLB, FinishLoadBalance), thisProxy));
+    }
+  } else {
+    CalculateMigrations();
+  }
+}
+
+void SelfishLB::MoveTasks(int who, int msg_size, int* tids, int* targets) {
+  for (int i = 0; i < msg_size; i++) {
+    task_to_destination_map.emplace(tids[i], targets[i]);
+  }
+  thisProxy[who].ConfirmTasks();
+}
+
+void ConfirmTasks() {
+  waiting_messages--;
+  if (!waiting_messages){
+    contribute(CkCallback(CkReductionTarget(SelfishLB, FinishLoadBalance), thisProxy));
+  }
+}
+
+void SelfishLB::FinishLoadBalance() {
+  for (auto task : all_tasks) {
+    if (std::get<0>(task) == CkMyPe() && std::get<1>(task) != CkMyPe()) {
+      // This is a leaving task
+      task_to_destination_map.emplace(std::get<1>(task), std::get<2>(task));
+    }
+  }
+  InsertMigrateInfo();
+}
+
+bool SelfishLB::DetermineMigrationSuccess(double host_load, double task_load) {
+  bool ret_val = false;
+  if (my_load + task_load > host_load) {
+    double chance = 1.0 - (my_load/host_load);
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+    ret_val = (chance > dis(gen));
+  }
+  return ret_val;
+}
+
+void SelfishLB::InsertMigrateInfo() {
+  for (auto task : task_to_destination_map) {
+    int tid = task.second; int destination = task.first;
+    MigrateInfo* inf = new MigrateInfo();
+    inf->obj = my_stats->objData[tid].handle;
+    inf->from_pe = CkMyPe();
+    inf->to_pe = destination;
+    migrateInfo.push_back(inf);
+    mig_counts[destination]++;
+  }
+
+  msg = new(task_to_destination_map.size(), CkNumPes(), CkNumPes(), 0) LBMigrateMsg;
+  msg->n_moves = task_to_destination_map.size();
+  for (size_t i = 0; i < task_to_destination_map.size(); ++i) {
+      MigrateInfo* inf = (MigrateInfo*) migrateInfo[i];
+      msg->moves[i] = *inf;
+      delete inf;
+  }
+  migrateInfo.clear();
+  for (int dest = 0; dest < CkNumPes(); dest++) {
+    thisProxy[dest].InformMigrations(mig_counts[dest]);
+  }
+  contribute(CkCallback(SelfishLB, FinalBarrier), thisProxy);
+}
+
+void FinalBarrier() {
+  ProcessMigrationDecisions(msg);
+}
+
+void InformMigrations(int count) {
+  migrates_expected += count;
 }
--- charm/algorithms/SelfishLB.h
+++ charm/algorithms/SelfishLB.h
@@ -18,14 +18,18 @@ public:
     SelfishLB(CkMigrateMessage *m);
     void ReceiveTask(int host_id, double host_load, int task_id, double task_load, int arr_position);
     void ConfirmRecv(bool recv, int who, int arr_position);
-    void MoveTasks(int msg_size,int task_id[], int destination[]);
+    void MoveTasks(int who, int msg_size,int task_id[], int destination[]);
+    void ConfirmTasks();
+    void InformMigrations(int count);
     void NextIteration(); // Reduction target, for iterations
+    void FinishLoadBalance();
+    void FinalBarrier();
 
 private:
    // Private functions
    void InitLB(const CkLBOptions &);
    void Strategy(const DistBaseLB::LDStats* const stats);
-   void InsertMigrateInfo(int task_id, int destination); // Fool proof migrateInfo cretion
+   void InsertMigrateInfo(); // Fool proof migrateInfo creation
 
    // Random Decision Process
    void CalculateMigrations(); // Decide which tasks will go where and issue the "ReceiveTask" calls
@@ -33,10 +37,11 @@ private:
 
    // Atributes
    std::vector<MigrateInfo*> migrateInfo;
-   std::vector< std::pair <int, int> > task_to_destination_map;
+   std::map<int, int> task_to_destination_map;
    std::vector< task_global_id_t > all_tasks;
+   std::vector<int> mig_counts;
    double my_load;
-   int waiting_messages;
+   int waiting_messages, cur_iteration;
 
   // Random Attributes
   std::random_device rd;  //Will be used to obtain a seed for the random number engine
@@ -44,6 +49,7 @@ private:
 
    // Magical constants
    int kIterations;
+   double kMigProb;
 
    // Charm Attributes
    LBMigrateMsg* msg;
